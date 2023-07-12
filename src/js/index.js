3fc9bbede1abafa48b89621806e0ee8641e3bba2
weslleyloureiro <79668546+weslleyloureiro@users.noreply.github.com>
2023-07-12 13:53:28 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+function calcular() {
+    var altura = document.getElementById("altura").value / 100;
+    var peso = document.getElementById("peso").value;
+
+    var imc = peso / altura ** 2;
+    var text = ""
+    if (imc < 18.5) {
+        text = "Você está magro"
+    } else if (imc < 24.9) {
+        text = "Você está no peso ideal"
+    } else if (imc < 29.9) {
+        text = "Você está com sobrepeso"
+    } else if (imc < 39.9) {
+        text = "Você está com obesidade"
+    } else if (imc > 39.9) {
+        text = "Você está com obesidade mórbida"
+    }
+    document.getElementById("text_area").innerText = text
+}
